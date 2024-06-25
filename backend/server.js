49675552a9697536c90eeb4c6b4a8054dc498dc6
smartdriver321 <tinggaldidisneyland@gmail.com>
2024-06-25 11:18:48 +0700

    Create home route

--- backend/server.js
+++ backend/server.js
@@ -6,15 +6,23 @@ const dotenv = require('dotenv').config()
 
 const app = express()
 const PORT = 5000
-const MONGO_URI =
-  'mongodb+srv://smart:admin@cluster0.uu2q5fd.mongodb.net/pinvent?retryWrites=true&w=majority&appName=Cluster0'
 
 // Connect to db and start server
 mongoose
-  .connect(MONGO_URI)
+  .connect(process.env.MONGO_URI)
   .then(() => {
     app.listen(PORT, () => {
       console.log(`Server running on port ${PORT}`)
     })
   })
   .catch((err) => console.log(err))
+
+// Middlewares
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+// Routes
+app.get('/', (req, res) => {
+  res.send('Home Page')
+})
