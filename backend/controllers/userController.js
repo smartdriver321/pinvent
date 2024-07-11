bb990bd99c286f424ed667080d2735bd28cc25a4
smartdriver321 <tinggaldidisneyland@gmail.com>
2024-07-11 19:40:49 +0700

    Register new user

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,7 +1,50 @@
-export const registerUser = (req, res) => {
-	if (!req.body.email) {
+import asyncHandler from 'express-async-handler'
+
+import { User } from '../models/userModel.js'
+import mongoose from 'mongoose'
+
+// Register User
+export const registerUser = asyncHandler(async (req, res) => {
+	const { name, email, password } = req.body
+
+	// Validation
+	if (!name || !email || !password) {
 		res.status(400)
-		throw new Error('Please add an email')
+		throw new Error('Please fill in all required fields')
 	}
-	res.send('Register User')
-}
+
+	if (password.length < 6) {
+		res.status(400)
+		throw new Error('Password must be up to 6 characters')
+	}
+
+	// Check if user email already exists
+	const userExists = await User.findOne({ email })
+
+	if (userExists) {
+		res.status(400)
+		throw new Error('Email has already been registered')
+	}
+
+	// Create new user
+	const user = await User.create({
+		name,
+		email,
+		password,
+	})
+
+	if (user) {
+		const { _id, name, email, photo, phone, bio } = user
+		res.status(201).json({
+			_id,
+			name,
+			email,
+			photo,
+			phone,
+			bio,
+		})
+	} else {
+		res.status(400)
+		throw new Error('Invalid user data')
+	}
+})
