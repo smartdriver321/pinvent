2fdba989e94fedd1f72074c6fd80457e34eae534
smartdriver321 <tinggaldidisneyland@gmail.com>
2024-07-11 18:18:35 +0700

    Create Home route

--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,22 @@ import express from 'express'
 import mongoose from 'mongoose'
 import cors from 'cors'
 import dotenv from 'dotenv'
+import bodyParser from 'body-parser'
 
 dotenv.config()
 
 const app = express()
 const PORT = process.env.PORT || 5000
+
+// Middlewares
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+// Home routes
+app.get('/', (req, res) => {
+	res.send('Home Page')
+})
 
 // Connect to DB and start server
 mongoose
